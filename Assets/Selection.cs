afa7aaa862ae370dd3bddcf58bf1488ee1e12d62
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-10-26 15:24:59 +0900

    Snap to angle

--- Assets/Selection.cs
+++ Assets/Selection.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 using UnityEngine;
 
@@ -22,6 +22,9 @@ public class Selection : MonoBehaviour {
     DOWN
   }
   State currentState = State.RISING;
+  float dragStartAngle;
+  float dragStartRotation;
+  float rotationVelocity;
 
   public State CurrentState {
     get { return currentState; }
@@ -31,7 +34,67 @@ public class Selection : MonoBehaviour {
     selected = new HashSet<Hex>();
   }
 
-  // Update is called once per frame
+  float AngleDiff(float start, float end) {
+    float normalizedStart = start;
+    float normalizedEnd = end;
+    // Rotate all angles into 0 ~ 180 segment to simplify diff calc
+    while (Mathf.Abs(normalizedEnd - normalizedStart) > 180) {
+      normalizedStart = (normalizedStart + 90) % 360f;
+      normalizedEnd = (normalizedEnd + 90) % 360f;
+    }
+    return normalizedEnd - normalizedStart;
+  }
+
+  float SnapRotation(float angle) {
+    return Mathf.Round((angle % 360f) / 60f) * 60f;
+  }
+
+  Vector3 MouseWorldPosition => Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+  Vector3 SelectionCenter => transform.TransformPoint(Vector3.zero);
+
+  Vector3 MouseOffsetFromSelectionCenter => MouseWorldPosition - SelectionCenter;
+
+  bool ClickedInSelectedRing {
+    get {
+      Vector3 offsetWithoutTransform = transform.InverseTransformVector(MouseOffsetFromSelectionCenter);
+      Vector3 clickPositionBeforeTransform = transform.parent.position + offsetWithoutTransform;
+      Hex clickedHex = map.HexAtPosition(clickPositionBeforeTransform);
+      return selected.Contains(clickedHex);
+    }
+  }
+
+  float MouseAngle {
+    get {
+      float result = Vector2.Angle(Vector2.up, MouseOffsetFromSelectionCenter);
+      if (MouseOffsetFromSelectionCenter.x > 0) {
+        result = 360f - result;
+      }
+      return result;
+    }
+  }
+
+  float CurrentRotation {
+    get { return transform.eulerAngles.z; }
+    set {
+      float newValue = value % 360f;
+      if (newValue < 0) {
+        newValue += 360f;
+      }
+      transform.eulerAngles = new Vector3(0, 0, newValue);
+    }
+  }
+
+  float TargetRotation {
+    get {
+      float dragRotation = AngleDiff(dragStartAngle, MouseAngle);
+      float snappedDragRotation = SnapRotation(dragRotation);
+      return dragStartRotation + snappedDragRotation;
+    }
+  }
+
+  float DiffToTargetRotation => AngleDiff(CurrentRotation, TargetRotation);
+
   void Update() {
     switch (currentState) {
       case State.RISING:
@@ -43,9 +106,18 @@ public class Selection : MonoBehaviour {
         {
           if (Input.GetMouseButtonDown(0) && ClickedInSelectedRing) {
             currentState = State.DRAGGING;
+            dragStartAngle = MouseAngle;
+            dragStartRotation = SnapRotation(CurrentRotation);
           } else if (Input.GetMouseButtonUp(0)) {
             currentState = State.SINKING;
           }
+          break;
+        }
+      case State.DRAGGING:
+        {
+          UpdateRotationVelocity();
+          CurrentRotation += rotationVelocity;
+          HandleSnap();
           break;
         }
       case State.SINKING:
@@ -56,16 +128,19 @@ public class Selection : MonoBehaviour {
     }
   }
 
-  bool ClickedInSelectedRing {
-    get {
-      Vector3 clickedPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-      Vector3 selectionCenter = transform.TransformPoint(Vector3.zero);
-      Vector3 offsetFromSelection = clickedPosition - selectionCenter;
+  void UpdateRotationVelocity() {
+    float force = DiffToTargetRotation;
+    float damping = -rotationVelocity * 10f;
+    rotationVelocity += Time.deltaTime * (force + damping);
+  }
 
-      Vector3 offsetWithoutTransform = transform.InverseTransformVector(offsetFromSelection);
-      Vector3 clickPositionBeforeTransform = transform.parent.position + offsetWithoutTransform;
-      Hex clickedHex = map.HexAtPosition(clickPositionBeforeTransform);
-      return selected.Contains(clickedHex);
+  void HandleSnap() {
+    if (Mathf.Abs(DiffToTargetRotation) < 1 && Mathf.Abs(rotationVelocity) < 0.1f) {
+      CurrentRotation = TargetRotation;
+      rotationVelocity = 0;
+      if (!Input.GetMouseButton(0)) {
+        currentState = State.UP;
+      }
     }
   }
 
